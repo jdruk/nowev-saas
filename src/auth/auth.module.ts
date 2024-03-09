e5d3463b0d7318fb36ab3609a742cb8ed2efbe90
Jonathan Muller <61502117+jonathanmullerr@users.noreply.github.com>
2024-03-09 20:49:15 +0000

    fixture: Fix base authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  providers: [AuthService, LocalStrategy],
-  imports: [UsersModule, PassportModule]
+  imports: [UsersModule],
+  providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  async validateUser(email: string): Promise<any>{
-    console.log('email', email)
-    const user = await this.usersService.findOneByEmail(email);
-    user ? user : null
+  async signIn(email: string): Promise<any> {
+    return await this.usersService.findOneByEmail(email);;
   }
 }
