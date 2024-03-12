caf150180e68b8275f30916520fad405c4bc9578
Jonathan Muller <61502117+jonathanmullerr@users.noreply.github.com>
2024-03-12 23:37:03 +0000

    fixture: Remove useless import

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,4 @@
-import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
